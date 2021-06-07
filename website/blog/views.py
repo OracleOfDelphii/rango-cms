909723660bb62aa2987900cf45bea89088e60b59
phalanthus <{k.bayest@yahoo.com}+{phalanthus}@users.noreply.github.com>
2021-06-07 23:43:01 +0430

    minor changes to implement restful api.

--- website/blog/views.py
+++ website/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
-from .serializers import CategorySerializer
+from .serializers import CategorySerializer, ArticleSerializer
+
 # Create your views here.
 
 from django.shortcuts import get_object_or_404, HttpResponseRedirect, get_list_or_404, redirect
@@ -64,11 +65,17 @@ def handle_uploaded_image(form, request):
 
 
 from rest_framework.viewsets import ModelViewSet
+import json
 
+# problem, need to break Panel into smaller pieces
+# problem, need to write serializer for every view including PANEL
 class Panel(ModelViewSet):
     renderer_classes = [TemplateHTMLRenderer]
     template_name = 'panel.html'
- 
+    #queryset = Article.objects.all()
+    #serializer_class = ArticleSerializer
+
+
     def get(self, request): 
         form = PostForm()
         return Response({'form': form})
