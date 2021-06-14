52a5a93006269b4f78bda1f52ff080a98c1b4f60
phalanthus <{k.bayest@yahoo.com}+{phalanthus}@users.noreply.github.com>
2021-06-14 22:55:33 +0430

    refactored code and added panel css files.

--- website/website/settings.py
+++ website/website/settings.py
@@ -135,16 +135,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 VENV_PATH = os.path.dirname(BASE_DIR)
 
-STATIC_ROOT = os.path.join(VENV_PATH, 'static_root')
-MEDIA_ROOT = os.path.join(VENV_PATH, 'media_root')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
-
-STATICFILES_DIRS = (
-os.path.join(BASE_DIR, "static"),
-)
-
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static_extra/'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
