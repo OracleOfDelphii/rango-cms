53ed7c6f9ceba196a61622d4e77bc52335bc4273
OracleOfDelphii <k.bayest@yahoo.com>
2022-06-06 03:06:30 +0430

    Removed serializers Removed Context_processor since those contexts are now embedded in class-based views themselves. Slight modification to templates and added a new comfirmation template for deleting articles. Customized plural names and names for model objects in admin panel.

--- website/blog/admin.py
+++ website/blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Category, Article, Article_Category
+admin.site.register ([Category, Article, Article_Category])
